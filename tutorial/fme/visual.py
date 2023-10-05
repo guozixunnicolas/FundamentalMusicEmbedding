ca3e1ebd2cd2ec4cf65349df2761dcf3e14da74a
Nicholas S <39043781+nichostst@users.noreply.github.com>
2023-10-05 21:21:22 +0700

    Optional text addition

--- tutorial/fme/visual.py
+++ tutorial/fme/visual.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot_distance_matrix(distance_matrix, notes, axis: plt.Axes):
+def plot_distance_matrix(distance_matrix, notes, axis: plt.Axes, add_text: bool = True):
     axis.matshow(distance_matrix,  cmap=plt.cm.Blues)
 
     axis.set_xticks(range(0, len(notes), 1))
@@ -9,9 +9,10 @@ def plot_distance_matrix(distance_matrix, notes, axis: plt.Axes):
     axis.set_xticklabels(notes[::1], fontsize=12, rotation=45)
     axis.set_yticklabels(notes[::1], fontsize=12, rotation=45)
 
-    for i in range(distance_matrix.shape[0]):
-        for j in range(distance_matrix.shape[1]):
-            axis.text(i, j, str(-distance_matrix[j,i])[:4], va='center', ha='center')
+    if add_text:
+        for i in range(distance_matrix.shape[0]):
+            for j in range(distance_matrix.shape[1]):
+                axis.text(i, j, str(-distance_matrix[j,i])[:4], va='center', ha='center')
 
 def plot_we_pitch_embedding(we_model):
     word_embedding = we_model.pitch_embedding.weight
